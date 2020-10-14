5dd7646a4c2b2cb92434c1fdda3cd673be745a80
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2020-10-14 16:06:41 -0700

    Real Errors do exist

--- tests/test_broto3.py
+++ tests/test_broto3.py
@@ -8,7 +8,7 @@ import pytest
 from broto3 import broto3
 
 
-def test_errors():
+def test_simulated_errors():
     session = broto3.BrokeSession(error_rate=1.0)
 
     with pytest.raises(Exception) as e_info:
@@ -23,3 +23,20 @@ def test_errors():
             ExpectedBucketOwner='string'
         )
     assert str(e_info.value).split(":")[-1].strip() == broto3.BrokeSession.error_message
+
+
+def test_real_errors():
+    session = broto3.BrokeSession(error_rate=0.0)
+
+    with pytest.raises(Exception) as e_info:
+        session.client("s3").list_objects(
+            Bucket='string',
+            Delimiter='string',
+            EncodingType='url',
+            Marker='string',
+            MaxKeys=123,
+            Prefix='string',
+            RequestPayer='requester',
+            ExpectedBucketOwner='string'
+        )
+    assert str(e_info.value).split(":")[-1].strip() != broto3.BrokeSession.error_message
