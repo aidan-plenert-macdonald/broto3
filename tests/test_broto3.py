923e8816e246ad16453e79c47942e4b5b9044a3c
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2020-10-14 15:31:37 -0700

    Basic error simulation

--- tests/test_broto3.py
+++ tests/test_broto3.py
@@ -8,17 +8,18 @@ import pytest
 from broto3 import broto3
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_errors():
+    session = broto3.BrokeSession(error_rate=1.0)
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    with pytest.raises(Exception) as e_info:
+        session.client("s3").list_objects(
+            Bucket='string',
+            Delimiter='string',
+            EncodingType='url',
+            Marker='string',
+            MaxKeys=123,
+            Prefix='string',
+            RequestPayer='requester',
+            ExpectedBucketOwner='string'
+        )
+    assert str(e_info.value).split(":")[-1].strip() == broto3.BrokeSession.error_message
